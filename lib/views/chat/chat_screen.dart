a9614ded40ff08969afd48fd21e7776f774555ef
Deepjyoti120 <deepjyoti120281@gmail.com>
2025-03-11 07:16:21 +0530

    done

--- lib/views/chat/chat_screen.dart
+++ lib/views/chat/chat_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:laatte/services/api_services.dart';
 import 'package:laatte/services/socket_services.dart';
+import 'package:laatte/ui/theme/container.dart';
+import 'package:laatte/ui/theme/text.dart';
+import 'package:laatte/ui/widgets/progress_circle.dart';
+import 'package:laatte/utils/design_colors.dart';
 import 'package:laatte/viewmodel/bloc/user_report_bloc.dart';
 
 class ChatMessages extends StatefulWidget {
@@ -17,6 +22,9 @@ class _ChatMessagesState extends State<ChatMessages> {
   final socketService = SocketService();
   final TextEditingController messageController = TextEditingController();
   List<Map<String, dynamic>> messages = [];
+  final FocusNode textFieldFocus = FocusNode();
+
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -43,6 +51,9 @@ class _ChatMessagesState extends State<ChatMessages> {
   }
 
   void sendMessage() async {
+    setState(() {
+      isLoading = true;
+    });
     if (messageController.text.isNotEmpty) {
       final userReportBloc = context.read<UserReportBloc>();
       final user = userReportBloc.state.userReport;
@@ -61,6 +72,10 @@ class _ChatMessagesState extends State<ChatMessages> {
       );
       messageController.clear();
     }
+
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -72,45 +87,149 @@ class _ChatMessagesState extends State<ChatMessages> {
 
   @override
   Widget build(BuildContext context) {
+    final user = context.read<UserReportBloc>().state.userReport;
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(title: const Text("Chat")),
-      body: Column(
-        children: [
-          Expanded(
-            child: messages.isEmpty
-                ? const Center(child: Text("No messages yet"))
-                : ListView.builder(
-                    itemCount: messages.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(messages[index]['message']),
-                        subtitle:
-                            Text("Sender: ${messages[index]['senderId']}"),
-                      );
-                    },
+      appBar: AppBar(title: const DesignText("Messages")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SizedBox(
+          height: 52,
+          width: double.infinity,
+          child: FloatingActionButton.extended(
+            onPressed: () async {},
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            backgroundColor: DesignColor.backgroundColorDarkMode,
+            elevation: 0,
+            label: SizedBox(
+              width: size.width - 40,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, top: 4),
+                child: TextField(
+                  controller: messageController,
+                  focusNode: textFieldFocus,
+                  decoration: InputDecoration(
+                    // fillColor: DesignColor.backgroundColorDarkMode,
+                    hintText: 'Type...',
+                    hintStyle: const TextStyle(color: Colors.white),
+                    border: InputBorder.none,
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(),
+                      onPressed: isLoading
+                          ? null
+                          : () async {
+                              if (messageController.text.isNotEmpty) {
+                                sendMessage();
+                              }
+                            },
+                      icon: Icon(
+                        isLoading ? FontAwesomeIcons.stop : Icons.send,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: messageController,
-                    decoration:
-                        const InputDecoration(hintText: "Type a message..."),
-                  ),
+                  onTapOutside: (event) {
+                    final currentFocus = FocusScope.of(context);
+                    if (currentFocus.focusedChild != null) {
+                      currentFocus.focusedChild!.unfocus();
+                    }
+                  },
                 ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: sendMessage,
-                ),
-              ],
+              ),
             ),
           ),
-        ],
+        ),
       ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: messages.length,
+        reverse: true,
+        padding: const EdgeInsets.fromLTRB(12, 12, 12, 120),
+        itemBuilder: (context, index) {
+          final data = messages.reversed.toList()[index];
+          bool isME = data['senderId'] == user?.id;
+          return Padding(
+            padding: isME
+                ? const EdgeInsets.fromLTRB(60, 12, 12, 6)
+                : const EdgeInsets.fromLTRB(12, 12, 60, 6),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment:
+                  isME ? MainAxisAlignment.end : MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Flexible(
+                  child: DesignContainer(
+                    color: isME
+                        ? DesignColor.blue2
+                        : DesignColor.backgroundColorDarkMode,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: isME
+                          ? const Radius.circular(30)
+                          : const Radius.circular(6),
+                      bottomRight: const Radius.circular(30),
+                      topLeft: const Radius.circular(30),
+                      topRight: isME
+                          ? const Radius.circular(6)
+                          : const Radius.circular(30),
+                    ),
+                    isColor: true,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
+                      child: DesignText(
+                        data['message'] ?? "",
+                        fontSize: 13,
+                        fontWeight: 700,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+      // body: Column(
+      //   children: [
+      //     Expanded(
+      //       child: messages.isEmpty
+      //           ? const Center(child: Text("No messages yet"))
+      //           : ListView.builder(
+      //               itemCount: messages.length,
+      //               shrinkWrap: true,
+      //               itemBuilder: (context, index) {
+      //                 return ListTile(
+      //                   title: Text(messages[index]['message']),
+      //                   subtitle:
+      //                       Text("Sender: ${messages[index]['senderId']}"),
+      //                 );
+      //               },
+      //             ),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Row(
+      //         children: [
+      //           Expanded(
+      //             child: TextField(
+      //               controller: messageController,
+      //               decoration:
+      //                   const InputDecoration(hintText: "Type a message..."),
+      //             ),
+      //           ),
+      //           IconButton(
+      //             icon: const Icon(Icons.send),
+      //             onPressed: sendMessage,
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
