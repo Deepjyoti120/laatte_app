60cba9297120d4757ab1bdc851b2a63683f932cb
Deepjyoti120 <deepjyoti120281@gmail.com>
2025-05-20 21:16:43 +0530

    Refactor ChatScreen and SettingScreen for improved layout and navigation

--- lib/views/chat/chat.dart
+++ lib/views/chat/chat.dart
@@ -34,84 +34,95 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     final chats = context.watch<SocketBloc>().state.chats;
     return Scaffold(
-      body: SafeArea(
-        child: ListView.builder(
-          itemCount: chats.length,
-          padding: const EdgeInsets.all(12),
-          itemBuilder: (context, index) {
-            final chat = chats[index];
-            return GestureDetector(
-              onTap: () async {
-                context
-                    .read<SocketBloc>()
-                    .add(SocketMessage(chatId: chat.id, setChatID: true));
-                context.push(Routes.chatMessages, extra: chat.id).then((e) {
-                  if (!mounted) return;
-                  // context
-                  //     .read<SocketBloc>()
-                  //     .add(const SocketMessage(setChatID: true,chatId: null));
-                  // chats = await ApiService().chats();
-                  // setState(() {});
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 6),
-                child: Card(
-                  color: DesignColor.backgroundColorDarkMode,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: DesignColor.yellow,
-                              width: 2,
-                            ),
-                          ),
-                          width: 54,
-                          height: 54,
-                          child: ClipRRect(
-                            clipBehavior: Clip.antiAlias,
-                            borderRadius: BorderRadius.circular(60),
-                            child: Image.network(
-                              chat.user?.profilePicture ?? "",
-                              alignment: Alignment.center,
-                              fit: BoxFit.fill,
-                            ),
+      appBar: AppBar(
+        title: const DesignText(
+          "Chats",
+          fontSize: 20,
+          fontWeight: 600,
+          // color: Colors.white,
+        ),
+        // centerTitle: true,
+        // backgroundColor: DesignColor.backgroundColorDarkMode,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: chats.length,
+        padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, index) {
+          final chat = chats[index];
+          return GestureDetector(
+            onTap: () async {
+              context
+                  .read<SocketBloc>()
+                  .add(SocketMessage(chatId: chat.id, setChatID: true));
+              context.push(Routes.chatMessages, extra: chat.id).then((e) {
+                if (!mounted) return;
+                // context
+                //     .read<SocketBloc>()
+                //     .add(const SocketMessage(setChatID: true,chatId: null));
+                // chats = await ApiService().chats();
+                // setState(() {});
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 6),
+              child: Card(
+                color: DesignColor.backgroundColorDarkMode,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: DesignColor.yellow,
+                            width: 2,
                           ),
                         ),
-                        8.width,
-                        Flexible(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
+                        width: 54,
+                        height: 54,
+                        child: ClipRRect(
+                          clipBehavior: Clip.antiAlias,
+                          borderRadius: BorderRadius.circular(60),
+                          child: Image.network(
+                            chat.user?.profilePicture ?? "",
+                            alignment: Alignment.center,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      8.width,
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            DesignText(
+                              chat.user?.name ?? "",
+                              fontSize: 16,
+                              fontWeight: 500,
+                              color: Colors.white,
+                            ),
+                            if (chat.lastMessage != null)
                               DesignText(
-                                chat.user?.name ?? "",
-                                fontSize: 16,
-                                fontWeight: 500,
+                                chat.lastMessage?.content ?? '',
+                                fontSize: 14,
+                                fontWeight: 400,
                                 color: Colors.white,
+                                maxLines: 2,
                               ),
-                              if (chat.lastMessage != null)
-                                DesignText(
-                                  chat.lastMessage?.content ?? '',
-                                  fontSize: 14,
-                                  fontWeight: 400,
-                                  color: Colors.white,
-                                  maxLines: 2,
-                                ),
-                            ],
-                          ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/views/settings/setting.dart
+++ lib/views/settings/setting.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:laatte/common_libs.dart';
+import 'package:laatte/routes.dart';
 import 'package:laatte/ui/theme/text.dart';
 import 'package:laatte/utils/design_colors.dart';
 import 'package:laatte/utils/extensions.dart';
@@ -18,20 +19,104 @@ class _SettingScreenState extends State<SettingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const DesignText.titleSemiBold("Settings"),
+                30.height,
+                const DesignText.body(
+                  "ACCOUNT",
+                  fontWeight: 600,
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Edit Profile",
+                  onTap: () {
+                    context.push(Routes.profileScreen);
+                  },
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Privacy Policy",
+                  onTap: () {},
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Notifications",
+                  onTap: () {},
+                ),
+                20.height,
+                const DesignText.body(
+                  "PREFERENCES",
+                  fontWeight: 600,
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Appearance",
+                  onTap: () {},
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Discovery Settings",
+                  onTap: () {},
+                ),
+                20.height,
+                const DesignText.body(
+                  "SUPPORT",
+                  fontWeight: 600,
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Give Feedback",
+                  onTap: () {},
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Help & Support",
+                  onTap: () {},
+                ),
+                const Divider(color: DesignColor.grey300),
+                settingsCard(
+                  title: "Logout",
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget settingsCard({
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return Material(
+      borderRadius: BorderRadius.circular(8),
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: onTap,
         child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
+          padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const DesignText.titleSemiBold("Settings"),
-              20.height,
-              const DesignText.body(
-                "ACCOUNT",
+              DesignText.body(
+                title,
                 fontWeight: 600,
+                color: DesignColor.grey900,
               ),
-              const Divider(color: DesignColor.grey300),
+              const Icon(
+                Icons.arrow_forward_ios,
+                color: DesignColor.grey500,
+              )
             ],
           ),
         ),
